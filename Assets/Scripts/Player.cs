a73e36b6cd3d5f9e841cc07acbb11d571e261dc0
sojeong0302 <01duqrl@naver.com>
2024-10-25 06:59:42 +0900

    feat: 무기 기능 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,34 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float moveSpeed;
+
+    [SerializeField]
+    private GameObject Weapon;
+
+    [SerializeField]
+    private Transform shootTransform; // 플레이어 머리 위로 발사되는 위치값
+
+    [SerializeField]
+    private float shootInterval = 0.05f; //미사일 쏘는 간격
+    private float lastShortTime = 0f; //마지막 쏜 시간
+
     void Update()
     {
         //키보드로 좌우 움직이기
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         Vector3 moveTo = new Vector3(horizontalInput, 0f, 0f); //2D라 Z값 의미X -> 0으로 지정
         transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        //Time.time: 게임이 시작된 이후로 현재까지 흐른 시간
+        if (Time.time - lastShortTime > shootInterval)
+        {
+            Instantiate(Weapon, shootTransform.position, Quaternion.identity);
+            lastShortTime = Time.time;
+        }
     }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] //유니티에게 접근 가능하게 해줌
+    private float moveSpeed = 10; //private 다른 클래스에서 접근x
+
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+    }
+    void Update()
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+    }
+}

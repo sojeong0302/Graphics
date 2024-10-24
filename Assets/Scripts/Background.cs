f2707fde1ed317fceb2426b09c5bcf64e64d684c
sojeong0302 <01duqrl@naver.com>
2024-10-25 05:49:37 +0900

    배경화면 c#

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private float moveSpeed = 3f; // 3f는 실수값이라 뒤에 f붙임
     void Update()
     {
-        
+        transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+
+        //background 위로 올려줌
+        if (transform.position.y < -10) { transform.position += new Vector3(0, 20f, 0); }
     }
 }
